4feb25852bd678e0da82db601d49e8ae0df07dab
cartwrightb <cartwrightb@gmail.com>
2022-12-29 20:55:22 +0000

    Updates to structure and created  main.js file

---
+++ frontend/main.js
@@ -0,0 +1,12 @@
+const functionAPI = '';
+
+const getVisitCount  = () => {
+    let count = 30;
+    fetch(functionAPI).then(response => {
+        return response.json()
+    }).then(response => {
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById
+    })
+}
